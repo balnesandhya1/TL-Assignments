cc639ffb7dbb0314b3349d8e0e2557786b3bd204
Balne Sandhya Rani <Balne Sandhya Rani>
2016-03-14 12:56:12 +0530

    added all practice tasks

---
+++ JS_tasks/js_events/t1.js
@@ -0,0 +1,10 @@
+<!Doctype html>
+<html>
+<head>
+<title>Javascript</title>
+</head>
+<body>
+<p id="demo">JavaScript can change HTML content</p>
+<button type="button" onclick="document.getElementById("demo")".innerHTML="hello JS">Click me</button>
+</body>
+</html>
